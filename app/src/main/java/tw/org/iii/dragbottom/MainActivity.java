fe9655d12fcc3677df0e4234e45844c4931abbd1
Chad Liu <tcfshchadnt@gmail.com>
2018-04-12 02:03:27 +0800

    V2

--- app/src/main/java/tw/org/iii/dragbottom/MainActivity.java
+++ app/src/main/java/tw/org/iii/dragbottom/MainActivity.java
@@ -34,11 +34,7 @@ public class MainActivity extends AppCompatActivity {
         click3.setOnClickListener(new MyClickListener());
     }
 
-    public void go(View view) {
-        view.getHeight();
 
-        test.setY(1800);
-    }
     private class MyClickListener implements View.OnClickListener{
 
         @Override
--- app/src/main/java/tw/org/iii/dragbottom/MyTestView.java
+++ app/src/main/java/tw/org/iii/dragbottom/MyTestView.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import java.util.zip.Inflater;
 
@@ -20,6 +21,10 @@ import java.util.zip.Inflater;
 
 public class MyTestView extends LinearLayout{
     private int actionBarHeight, vHeight;
+    private ScrollView sv;
+    private boolean isDrag;
+    float postion;
+    private float handInt;
     public MyTestView(Context context){
         super(context);
 
@@ -28,6 +33,7 @@ public class MyTestView extends LinearLayout{
         super(context, attrs);
         LayoutInflater mInflater = LayoutInflater.from(context);
         View myView = mInflater.inflate(R.layout.test, null);
+        sv =myView.findViewById(R.id.sv);
         addView(myView);
         //取得螢幕寬高
         DisplayMetrics dm = new DisplayMetrics();
@@ -35,7 +41,9 @@ public class MyTestView extends LinearLayout{
         a1.getWindowManager().getDefaultDisplay().getMetrics(dm);
         int vWidth = dm.widthPixels;
         vHeight= dm.heightPixels;
-        this.setOnTouchListener(new MyOnTouchListener());
+//        this.setOnTouchListener(new MyOnTouchListener());
+        isDrag =true;
+
 
     }
 
@@ -47,33 +55,100 @@ public class MyTestView extends LinearLayout{
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        Log.v("chad","onLayout");
+//        Log.v("chad","onLayout");
         super.onLayout(changed, l, t, r, b);
-        actionBarHeight =vHeight-this.getHeight();
+        actionBarHeight =320;
         this.setY(1000);
 
     }
 
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        postion =event.getRawY()-actionBarHeight;
+////        Log.v("chad",postion+" = onTouchEvent");
+////        Log.v("chad",event.getAction()+"");
+////        if (postion < 1848 && postion > 1000) {
+////
+////            this.setY(postion);
+//        float temp = handInt-event.getRawY();
+//        if(temp>0){
+//            this.setY(1000);
+//            handInt=event.getRawY();
+//            return true;
+//
+//        }else if (temp<0){
+//            this.setY(1800);
+//            handInt=event.getRawY();
+//            return false;
+//        }
+//        return false;
+////
+////        }else{
+////            if(postion<1000) {
+////                isDrag = false;
+////            }
+////            return false;
+////        }
+//    }
+
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-//        Log.v("chad","Y= "+this.getY());
-//        Log.v("chad","eventY ="+event.getRawY());
-        //取得ActionBar 高度
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
 
 
-        float postion = event.getRawY()-actionBarHeight;
-        if(postion<1848&&postion>1000) {
-            this.setY(postion);
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            Log.v("chad","ACTION_DOWN");
+            handInt =ev.getRawY();
+        } else if (ev.getAction() == MotionEvent.ACTION_MOVE) {
+            Log.v("chad",sv.getScrollY()+"");
+
+                float temp = handInt - ev.getRawY();
+//            Log.v("chad",temp+"");
+                if (temp > 0) {
+                    handInt = ev.getRawY();
+                    Log.v("chad", "ACTION_MOVE_up");
+                    if(this.getY()==1000) {
+                        return false;
+                    }else{
+                        this.setY(1000);
+                    }
+
+                } else if (temp < 0) {
+                    Log.v("chad", "ACTION_MOVE_down");
+
+                    handInt = ev.getRawY();
+                    if(this.getY()==1000&&sv.getScrollY()==0) {
+                        this.setY(1800);
+
+                    }else{
+                        return false;
+                    }
+                }
+
+
+            return true;
         }
-        return true/*super.onTouchEvent(event)*/;
+
+
+        return false;
+
+
+
+
+
+
+
+
+
+         /*super.onInterceptTouchEvent(ev)*/
     }
-    private class MyOnTouchListener implements OnTouchListener{
 
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-
-
-            return false;
-        }
-    }
+//    private class MyOnTouchListener implements OnTouchListener{
+//
+//        @Override
+//        public boolean onTouch(View v, MotionEvent event) {
+//            Log.v("chad","onTouch");
+//
+//            return true;
+//        }
+//    }
 }
